7129a80f290a74768ca9433ebd42fb2364f7d627
Anh Huy <tranbaoanhhuy6@gmail.com>
2024-05-24 09:50:35 +0700

    firstcommit

---
+++ middleware/translator.go
@@ -0,0 +1,25 @@
+package middleware
+
+import (
+	"github.com/anhhuy1010/cms-menu/config"
+	"github.com/anhhuy1010/cms-menu/helpers/translator"
+	"github.com/gin-gonic/gin"
+)
+
+func Translator() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		locale := c.Request.Header.Get("X-LANG")
+		if locale != "" {
+			if !translator.IsLocaleSupported(locale) {
+				cfg := config.GetConfig()
+				locale = cfg.GetString("server.locale")
+			}
+		} else {
+			cfg := config.GetConfig()
+			locale = cfg.GetString("server.locale")
+		}
+		c.Set("locale", locale)
+
+		c.Next()
+	}
+}
